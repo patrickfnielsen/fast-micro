425b5aa283e28e802a6b076bd0886d826a87b107
Patrick Falk Nielsen <patrick@6k.io>
2021-11-02 15:54:27 +0100

    feat: allow context plugins to be used

--- fast_micro/__init__.py
+++ fast_micro/__init__.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from starlette.middleware import Middleware
 from starlette_context import plugins
 from starlette_context.middleware import RawContextMiddleware
+from starlette_context.plugins.base import Plugin
 from fast_micro.logger import setup_logging
 from fast_micro.middleware import RequestEncrichMiddleware
 
@@ -11,17 +12,22 @@ from fast_micro.middleware import RequestEncrichMiddleware
 def create_app(
         log_level: str = "INFO", skip_route_logging: List[str] = None, 
         health_url: str = "/health", middleware: List[Middleware] = None,
-        logging_config: Optional[dict] = None
+        context_plugins: List[Plugin] = None, logging_config: Optional[dict] = None
     ) -> FastAPI:
 
     setup_logging(log_level, logging_config)
 
+    app_context_plugins = (
+        plugins.RequestIdPlugin(),
+        plugins.CorrelationIdPlugin(),
+    )
+
+    if context_plugins:
+        app_context_plugins = app_context_plugins + context_plugins
+
     skip_route_logging = skip_route_logging if skip_route_logging else [health_url]
     app_middleware = [
-        Middleware(RawContextMiddleware, plugins=(
-            plugins.RequestIdPlugin(),
-            plugins.CorrelationIdPlugin(),
-        )),
+        Middleware(RawContextMiddleware, plugins=app_context_plugins),
         Middleware(RequestEncrichMiddleware, skip_routes=skip_route_logging),
     ]
     
